25664dd2eb585b199f1253f7144f28b92c0ee58f
UiVGray <gray.777777@gmail.com>
2018-02-22 02:08:04 +0300

    Task 2:Omegalul

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let len = 0;
+  let set = new Set(preferences);
+  for(let i of set.values()) {
+    len++;  
+  }
+  return (len / 3);  // your implementation
 };
