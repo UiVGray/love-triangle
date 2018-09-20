b757af5650b377b2ce93538a94b5f782d660ed6b
UiVGray <gray.777777@gmail.com>
2018-09-20 16:53:04 +0300

    Task 2:feature

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let len = 0;
-  let set = new Set(preferences);
-  for(let i of set.values()) {
-    len++;  
+    for (let i = 0, lenght = preferences.length; i < lenght; i++) {
+      const req1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+      const req2 = i + 1 !== preferences[i];
+      if (req1 && req2) {
+          len++;
+      }
   }
-  return (len / 3);  // your implementation
+  return (len / 3);
 };
